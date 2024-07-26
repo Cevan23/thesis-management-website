9b9af46b4e19ab2ae7f9e050ab450acb186b67ef
Cevan23 <147426451+Hocjk@users.noreply.github.com>
2024-07-26 20:37:23 +0700

    update skeleton profile page

--- frontend/src/components/pages/Profile.jsx
+++ frontend/src/components/pages/Profile.jsx
@@ -1,21 +1,51 @@
-import React from 'react';
-import { Container, Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { Container, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
   const navigate = useNavigate();
 
   const goToHomePage = () => {
-    navigate('/');
+    navigate("/");
   };
 
   return (
     <Container className="mt-4">
-      <h1>Profile Page</h1>
-      <p>This is the Profile page content.</p>
-      <Button variant="primary" onClick={goToHomePage}>Go to Home Page</Button>
+      <h2>Profile</h2>
+      <form className="space-x-4 space-y-4 space-x-reverse">
+        <div className="space-x-4 space-y-4 space-x-reverse">
+          <label>Email:</label>
+          <input type="email" name="email" readOnly />
+        </div>
+        <div className="space-x-4 space-y-4 space-x-reverse">
+          <label>Password:</label>
+          <input type="password" name="password" className="caret-pink-500 border-2 border-rose-600"/>
+        </div>
+        <div className="space-x-4 space-y-4 space-x-reverse">
+          <label>First Name:</label>
+          <input type="text" name="name" className="caret-pink-500 border-2 border-rose-600"/>
+        </div>
+        <div className="space-x-4 space-y-4 space-x-reverse">
+          <label>Last Name:</label>
+          <input type="text" name="lastname"  className="caret-pink-500 border-2 border-rose-600"/>
+        </div>
+        <div className="space-x-4 space-y-4 space-x-reverse"> 
+          <label>Role:</label>
+          <input type="text" name="role" readOnly />
+        </div>
+        <div className="flex-1 space-x-2" >
+          <Button className="space-x-2 caret-pink-500" variant="primary" onClick={goToHomePage}>
+            Go to Home Page
+          </Button>
+
+          <Button className="space-x-2 caret-pink-500" variant="primary" onClick={goToHomePage}>
+            Update Profile
+          </Button>
+        </div>
+        
+      </form>
     </Container>
   );
-}
+};
 
 export default Profile;
